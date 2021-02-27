238e6f200f2af6448e0e9f47bbfb6baeb0581388
Nadezhda O <Obsholova.n@gmail.com>
2021-02-27 14:52:08 +0300

    калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,30 @@
+/*Создайте объект calculator (калькулятор) с тремя методами:
+
+read(a, b) (читать) запрашивает два значения и сохраняет их как свойства объекта.
+sum() (суммировать) возвращает сумму сохранённых значений.
+mul() (умножить) перемножает сохранённые значения и возвращает результат.
 let calculator = {
-  // ваш код
+// ... ваш код ...
 };
+calculator.read(3, 5);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul()); // 15
+*/
+
+let calculator = {
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator[read](3, 5);
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
